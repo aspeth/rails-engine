1826cdbd5dfbb24f72f7cdb250d435369caf924f
Andrew Speth <speth.andrew@yahoo.com>
2022-05-17 13:47:31 -0600

    test: GET '/api/v1/items/id' gets one item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -29,4 +29,29 @@ RSpec.describe "Item API requests" do
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
     end
   end
+
+  it "gets one item" do
+    merchant_id = create(:merchant).id
+    id = create(:item, merchant_id: merchant_id).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item.count).to eq(1)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+    
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
+  end
 end
