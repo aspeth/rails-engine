da7ebcd14b06ac787f5bba32531e2e50b633bf14
Andrew Speth <speth.andrew@yahoo.com>
2022-05-16 21:38:53 -0600

    Part 1, Section 1a

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,56 @@ RSpec.describe "Merchant API requests" do
     get "/api/v1/merchants"
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    
+    expect(merchants.count).to eq(5)
+
+    merchants.each do |merchant|
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it "finds one merchant" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchant.count).to eq(1)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it "finds one merchant's items" do
+    id = create(:merchant).id
+    items = create_list(:item, 10, merchant_id: id)
+
+    get "/api/v1/merchants/#{id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items.count).to eq(10)
+    
+    items.each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
   end
 end
