6a21534d6371379c7cc2c847ac5e1edb4f7d051d
Andrew Speth <speth.andrew@yahoo.com>
2022-05-17 13:40:30 -0600

    test: GET '/api/v1/items' gets all items

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Item API requests" do
+  it "gets all items" do
+    id_1 = create(:merchant).id
+    id_2 = create(:merchant).id
+    items_1 = create_list(:item, 10, merchant_id: id_1)
+    items_2 = create_list(:item, 5, merchant_id: id_2)
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items.count).to eq(15)
+
+    items.each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
+  end
+end
