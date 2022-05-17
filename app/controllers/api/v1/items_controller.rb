9f4455594cd35f3ed80a868e1a5b1fa32f4f63f4
Andrew Speth <speth.andrew@yahoo.com>
2022-05-17 13:41:01 -0600

    feat: GET '/api/v1/items' gets all items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(merchant.items)
+    items = Item.all
+    render json: ItemSerializer.new(items)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/merchants/:merchant_id/items', to: 'merchant_items#index'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
       end
+
+      resources :items, only: [:index]
     end
   end
 end
